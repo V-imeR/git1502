3049217e55915299e1b03aa2d655d26538caab4f
Ivan Shandruin <i.d.rastorguev@gmail.com>
2026-02-15 18:35:34 +0300

    mod2 test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 print('Hellow!')
 a = int(input('Введите число: \n'))
 b = int(input('Введите число: \n'))
-c = int(input('Введите третье число: \n'))
+c = int(input('Введите число: \n'))
 
 print(a)
 print(b)
+print(c)

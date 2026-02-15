559b2f5d2c88d7e536d77f1efb59356012fe1ff9
Ivan Shandruin <i.d.rastorguev@gmail.com>
2026-02-15 18:33:46 +0300

    mod test.py

--- test.py
+++ test.py
@@ -1,16 +1,7 @@
 print('Hellow!')
 a = int(input('Введите число: \n'))
 b = int(input('Введите число: \n'))
-<<<<<<< HEAD
-<<<<<<< HEAD
 c = int(input('Введите третье число: \n'))
 
-
-=======
 print(a)
 print(b)
->>>>>>> iss11
-=======
-print(a)
-print(b)
->>>>>>> iss11

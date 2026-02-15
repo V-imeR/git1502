6a2a3fd6b6162b94b286fec3f94ee33db12288b5
Ivan Shandruin <i.d.rastorguev@gmail.com>
2026-02-15 22:57:59 +0300

    edit test,py

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ a1 = 4
 a = int(input('Введите число: \n'))
 b = int(input('Введите число: \n'))
 c = int(input('Введите число: \n'))
+e = int(input('Введите число: \n'))
 
-print(a + b + c)
+print(a + b + c * 2 - e)
 print('Hellow!')
 print('Hellow!')
 print('Hellow word!')
 print('Hellow word!')
+print('Hellow!')
 print('Hellow word!')

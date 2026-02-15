123edf0fbb36f7494524ee519d10cf1ba5fdd65c
Ivan Shandruin <i.d.rastorguev@gmail.com>
2026-02-15 18:30:34 +0300

    eddit3 test.py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 print('Hellow!')
 a = int(input('Введите число: \n'))
 b = int(input('Введите число: \n'))
+<<<<<<< HEAD
 c = int(input('Введите третье число: \n'))
 
+
+=======
 print(a)
+print(b)
+>>>>>>> iss11
